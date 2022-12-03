6c39eb0b641f6e822ed7c48620864efa888edca5
Евгений Клюев <craftycoderpro@gmail.com>
2022-12-03 18:46:11 +0300

    fix pagination.py

--- backend/backend/pagination.py
+++ backend/backend/pagination.py
@@ -8,10 +8,8 @@ class PageCountPagination(PageNumberPagination):
 
     def get_paginated_response(self, data):
         return Response({
-            'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link()
-            },
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
             'count': self.page.paginator.count,
             'results': data
         })
