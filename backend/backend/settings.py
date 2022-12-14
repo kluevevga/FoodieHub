6317a1b5d49a865187bed807582d4a61d51c27d0
Евгений Клюев <craftycoderpro@gmail.com>
2022-12-14 22:09:10 +0300

    add config cfg

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ('foodgram-project.dynnamn.ru',)
 
 ROOT_URLCONF = 'backend.urls'
 AUTH_USER_MODEL = 'users.User'
